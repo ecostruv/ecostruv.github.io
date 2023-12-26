db5ac26e2d98ae7233ab2f57b672e39ff14ef268
Andrew Kim <107439239+i30101@users.noreply.github.com>
2023-12-25 20:55:30 -0500

    temp

---
+++ js/team.js
@@ -0,0 +1,39 @@
+
+const modal1 = document.getElementById("modal1");
+const input1 = document.getElementById("member1");
+
+modal1.addEventListener("shown.bs.modal", () => {
+    input1.focus();
+})
+
+
+const modal2 = document.getElementById("modal2");
+const input2 = document.getElementById("member2");
+
+modal1.addEventListener("shown.bs.modal", () => {
+    input1.focus();
+})
+
+
+const modal3 = document.getElementById("modal3");
+const input3 = document.getElementById("member3");
+
+modal1.addEventListener("shown.bs.modal", () => {
+    input1.focus();
+})
+
+
+const modal4 = document.getElementById("modal4");
+const input4 = document.getElementById("member4");
+
+modal1.addEventListener("shown.bs.modal", () => {
+    input1.focus();
+})
+
+
+const modal5 = document.getElementById("modal5");
+const input5 = document.getElementById("member5");
+
+modal1.addEventListener("shown.bs.modal", () => {
+    input1.focus();
+})
