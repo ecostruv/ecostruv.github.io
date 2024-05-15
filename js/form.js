900c7efc9b00a4418f4a2cfdfb312afa9db4d835
Andrew Kim <107439239+i30101@users.noreply.github.com>
2024-05-15 10:40:16 -0400

    redirect complete

--- js/form.js
+++ js/form.js
@@ -1,7 +1,25 @@
 // handles redirects for form submissions
 
-function formSubmit() {
-    setTimeout(() => {
-        window.location.replace("https://ecostruv.tech/success")
-    }, 1000);
-}
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById("contact-form", function(e) {
+        e.preventDefault();
+
+        var data = new FormData(form);
+
+        fetch(form.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                window.location.href = "https://ecostruv.tech/success";
+            } else {
+                alert("Oops! There was an erorr submitting your form.");
+            }
+        }).catch(error => {
+            alert("Oops! Second error message");
+        })
+    })
+});
